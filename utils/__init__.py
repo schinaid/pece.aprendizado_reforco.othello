a26eec41bc15ff9d1e0797401f19efb588149ce3
schinaid <andersonschinaid@hotmail.com>
2025-07-29 17:30:09 +0000

    adicionando modulo

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+"""
+Módulo de inicialização do pacote do jogador de Othello.
+Equipe: Anderson Alves Schinaid, Jorge Filho e Simone Britto.
+"""
--- utils/avaliador.py
+++ utils/avaliador.py
@@ -1,28 +1,9 @@
-
-import numpy as np
-
-def mobilidade(jogo, jogador):
-    '''
-    Calcula o número de jogadas legais do jogador.
-    '''
-    jogo_clone = jogo if jogo.jogador_atual() == jogador else jogo.joga(None)
-    return len(jogo_clone.jogadas_legais())
+"""
+Funções auxiliares de avaliação para o agente de Othello.
+Equipe: Anderson Alves Schinaid, Jorge Filho e Simone Britto.
+"""
 
 def diferenca_mobilidade(jogo, jogador):
-    '''
-    Calcula a diferença entre jogadas legais do jogador e do oponente.
-    '''
-    return mobilidade(jogo, jogador) - mobilidade(jogo, -jogador)
-
-def vantagem_posicional(tabuleiro, pesos):
-    '''
-    Aplica pesos a cada posição do tabuleiro e calcula soma total.
-
-    Argumentos:
-    - tabuleiro: numpy array 8x8
-    - pesos: numpy array 8x8
-
-    Retorno:
-    - valor escalar da soma ponderada
-    '''
-    return np.sum(tabuleiro * pesos)
+    movimentos_jogador = len(jogo.movimentos_validos(jogador))
+    movimentos_oponente = len(jogo.movimentos_validos(1 - jogador))
+    return movimentos_jogador - movimentos_oponente
